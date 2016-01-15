cf90f375bdad37c0d9ce76a186c120d7c369a9d4
lcts <ce@lcts.de>
2016-01-15 16:30:31 +0100

    minor changes

--- private/dat2load.m
+++ private/dat2load.m
@@ -18,9 +18,9 @@ if fid == -1
     error('dat2load:OpenFailed','fopen() failed to open ''%s''. Error message:\n %s.',filename, errmsg)
 end
 % check if file is binary
-line = fread(fid,256);
+fileline = fread(fid,256);
 % find non-printable characters except newline, carriage return, tab
-if any( line > 126 & line < 32 & line ~= 13 & line ~= 10 & line ~= 9 )
+if any( fileline > 126 & fileline < 32 & fileline ~= 13 & fileline ~= 10 & fileline ~= 9 )
     error('dat2load:Binary','This looks like a binary file.')
 end
 frewind(fid);
@@ -38,42 +38,42 @@ is2ch = false; isch1 = false; isakku = false; ismeta = false;
 noch2 = false;
 % loop through lines in file
 while ~feof(fid)       % while not at end of file
-    line = fgetl(fid); % get a new line, including \n
+    fileline = fgetl(fid); % get a new line, including \n
     linenumber = linenumber + 1;
     % 
     % GET FILE KEYWORDS AND DATE
     %
-    if length(line) >= 2
-        if strcmp(line(1:2),'%?')
-            if ~isempty(regexp(line, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', 'once'))
+    if length(fileline) >= 2
+        if strcmp(fileline(1:2),'%?')
+            if ~isempty(regexp(fileline, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', 'once'))
                 % try to read date/time in format
                 % '%? <YYYY-MM-DD hh:mm:ss>'
-                date = sscanf(line, '%%? %4d-%2d-%2d %2d:%2d:%2d');
-            elseif ~isempty(regexp(line,'addcols','once'))
+                date = sscanf(fileline, '%%? %4d-%2d-%2d %2d:%2d:%2d');
+            elseif ~isempty(regexp(fileline,'addcols','once'))
                 % check if a line defines additional columns besides data
                 % delete the '%?' and 'addcols' from the cell array
-                addcols = textscan(line, '%s'); addcols = addcols{1}; addcols(1:2) = [];
+                addcols = textscan(fileline, '%s'); addcols = addcols{1}; addcols(1:2) = [];
             else
                 % check for known file id strings
                 % do this last, so you don't match these strings in time/date or
                 % addcols lines
-                if ~isempty(regexp(line,'1d',  'once')); is1d   = true; if strcmp(ftype, ''); ftype = {'1d'}; else ftype = strcat(ftype, {' '}, '1d'); end; end
-                if ~isempty(regexp(line,'2d',  'once')); is2d   = true; if strcmp(ftype, ''); ftype = {'2d'}; else ftype = strcat(ftype, {' '}, '2d'); end; end
-                if ~isempty(regexp(line,'2ch', 'once')); is2ch  = true; if strcmp(ftype, ''); ftype = {'2ch'}; else ftype = strcat(ftype, {' '}, '2ch'); end; end
-                if ~isempty(regexp(line,'ch1', 'once')); isch1  = true; if strcmp(ftype, ''); ftype = {'ch1'}; else ftype = strcat(ftype, {' '}, 'ch1'); end; end
-                if ~isempty(regexp(line,'ch2', 'once')); if strcmp(ftype, ''); ftype = {'ch2'}; else ftype = strcat(ftype, {' '}, 'ch2'); end; end
-                if ~isempty(regexp(line,'akku','once')); isakku = true; if strcmp(ftype, ''); ftype = {'akku'}; else ftype = strcat(ftype, {' '}, 'akku'); end; end
-                if ~isempty(regexp(line,'meta','once')); ismeta = true; if strcmp(ftype, ''); ftype = {'meta'}; else ftype = strcat(ftype, {' '}, 'meta'); end; end
+                if ~isempty(regexp(fileline,'1d',  'once')); is1d   = true; if strcmp(ftype, ''); ftype = {'1d'}; else ftype = strcat(ftype, {' '}, '1d'); end; end
+                if ~isempty(regexp(fileline,'2d',  'once')); is2d   = true; if strcmp(ftype, ''); ftype = {'2d'}; else ftype = strcat(ftype, {' '}, '2d'); end; end
+                if ~isempty(regexp(fileline,'2ch', 'once')); is2ch  = true; if strcmp(ftype, ''); ftype = {'2ch'}; else ftype = strcat(ftype, {' '}, '2ch'); end; end
+                if ~isempty(regexp(fileline,'ch1', 'once')); isch1  = true; if strcmp(ftype, ''); ftype = {'ch1'}; else ftype = strcat(ftype, {' '}, 'ch1'); end; end
+                if ~isempty(regexp(fileline,'ch2', 'once')); if strcmp(ftype, ''); ftype = {'ch2'}; else ftype = strcat(ftype, {' '}, 'ch2'); end; end
+                if ~isempty(regexp(fileline,'akku','once')); isakku = true; if strcmp(ftype, ''); ftype = {'akku'}; else ftype = strcat(ftype, {' '}, 'akku'); end; end
+                if ~isempty(regexp(fileline,'meta','once')); ismeta = true; if strcmp(ftype, ''); ftype = {'meta'}; else ftype = strcat(ftype, {' '}, 'meta'); end; end
             end
         %
         % GET PARAMETERS
         %
-        elseif strcmp(line(1:2),'%!')
+        elseif strcmp(fileline(1:2),'%!')
             % increment 'parameter' counter
             jj = jj + 1;
             % try to read parameter in format
             % '%! <parname> <parvalue> <parunit>'
-            paramsraw{jj} = textscan(line, '%s %s %f %s');
+            paramsraw{jj} = textscan(fileline, '%s %s %f %s');
             % if it does not work
             if isempty(paramsraw{jj}{2}) || isempty(paramsraw{jj}{3})
                 % increment 'not parsed' counter
@@ -93,11 +93,11 @@ while ~feof(fid)       % while not at end of file
         %
         % GET COMMENTS
         %
-        elseif strcmp(line(1:2),'%.')
+        elseif strcmp(fileline(1:2),'%.')
             % increment 'comment' counter
             kk = kk + 1;
             % don't care what's in comments, read everything after '%. '
-            comment{kk} = line(3:end);
+            comment{kk} = fileline(3:end);
         else
             % increment 'not parsed' counter
             ll = ll + 1;
