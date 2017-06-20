6b135bb8fd0c2aa3b2e30ecc173715cc3821e0f5
lcts <ce@lcts.de>
2017-06-20 14:25:30 +0200

    fixed dim -> size, bstep -> bstart

--- private/LoadMagnetecXML.m
+++ private/LoadMagnetecXML.m
@@ -17,17 +17,17 @@ data = data(isfinite(data(:, 1)), :);
 pars.mwfreq = paramstemp.Measurement_MwFreq * 1e9; % Hz
 pars.pwr    = paramstemp.MicrowavePower * 1e-3;    % W
 pars.modamp = paramstemp.Modulation * 10;          % G
-pars.nscans = paramstemp.Accumulations;
+%pars.nscans = paramstemp.Accumulations;
 pars.T      = paramstemp.Measurement_Temperature + 273.15; % K
 if isfield(paramstemp, 'EnableQFactorMeasurement') && strcmp(paramstemp.EnableQFactorMeasurement,'True')
 	pars.q  = paramstemp.Measurement_QFactor;
 end
-pars.tc       = paramstemp.SweepTime / (dim(data, 1) * 1e-3);
+%pars.tc       = paramstemp.SweepTime / (size(data, 1) * 1e-3);
 % these sc doesn't need, but let's load them anyway,
 % so that this function is also useful for other purposes
 pars.bstart   = paramstemp.Bfrom * 10;             % G
 pars.bstop    = paramstemp.Bto * 10;               % G
 pars.li_freq  = paramstemp.ModulationFreq * 1000;  % kHz
 pars.li_phase = paramstemp.Measurement_Phase;      % °
-pars.npoints  = dim(data, 1);
-pars.bstep    = abs(pars.bstop - pars.bstep) / (pars.npoints - 1);
+pars.npoints  = size(data, 1);
+pars.bstep    = abs(pars.bstop - pars.bstart) / (pars.npoints - 1);
